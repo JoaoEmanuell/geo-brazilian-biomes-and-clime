d46d5580adc3c3ea124631b3a1edec37b1229504
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-09-19 18:03:09 -0300

    Initial react files

---
+++ gbbc/src/app/biomes/page.tsx
@@ -0,0 +1,11 @@
+import { CenterDiv } from '@/components/body/CenterDiv'
+
+export default function BiomesPage() {
+    return (
+        <main>
+            <CenterDiv>
+                <h1>Biomas brasileiros</h1>
+            </CenterDiv>
+        </main>
+    )
+}
