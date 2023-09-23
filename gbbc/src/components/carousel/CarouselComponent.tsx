bc68572bd8e18c08731f7a809806b32a07d68eb9
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-09-23 15:54:37 -0300

    refactor: Carousel

--- gbbc/src/components/carousel/CarouselComponent.tsx
+++ gbbc/src/components/carousel/CarouselComponent.tsx
@@ -1,6 +1,5 @@
+import { Carousel, Typography } from '@material-tailwind/react'
 import Image from 'next/image'
-import 'bootstrap/dist/css/bootstrap.css'
-import Carousel from 'react-bootstrap/Carousel'
 
 export interface CarrouselItemObjectInterface {
     src: string // Full path
@@ -13,34 +12,54 @@ export interface CarouselComponentInterface {
     carrouselItems: Array<CarrouselItemObjectInterface>
 }
 
-export function CarouselComponent(props: CarouselComponentInterface) {
-    const carouselItemImageWidth = 1080
+export function ReactCarousel(props: CarouselComponentInterface) {
+    const carouselItemImageWidth = 1280
     const carouselItemImageHeight = 720
-    const carouselItemInterval = 6000
+    const constructBaseTitleDiv = (title?: string) => {
+        if (title) {
+            return (
+                <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/25">
+                    <div className="w-3/4 text-center md:w-2/4">
+                        <Typography
+                            variant="h1"
+                            color="white"
+                            className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                        >
+                            {title}
+                        </Typography>
+                    </div>
+                </div>
+            )
+        } else {
+            return <div></div>
+        }
+    }
 
     const carouselItems = props.carrouselItems
-    var itemsComponent: Array<Carousel.Item> = []
+    var itemsComponent: Array<React.JSX.Element> = []
     carouselItems.forEach((item) => {
         const itemComponent = (
-            <Carousel.Item interval={carouselItemInterval}>
+            <div className="relative h-full w-full">
                 <Image
                     src={item.src}
                     alt={item.alt}
                     width={carouselItemImageWidth}
                     height={carouselItemImageHeight}
-                    className="block w-100"
+                    className="object-scale-down h-64 w-full"
                 />
-                <Carousel.Caption>
-                    <h3>{item.captionTitle}</h3>
-                    <p>{item.captionText}</p>
-                </Carousel.Caption>
-            </Carousel.Item>
+                {constructBaseTitleDiv(item.captionTitle)}
+            </div>
         )
         itemsComponent.push(itemComponent)
     })
     return (
-        <Carousel fade pause="hover">
-            {...itemsComponent}
-        </Carousel>
+        <div className="px-4">
+            <Carousel
+                transition={{ duration: 1.5 }}
+                className="rounded-xl h-64 w-full"
+            >
+                {...itemsComponent}
+            </Carousel>
+        </div>
     )
 }
