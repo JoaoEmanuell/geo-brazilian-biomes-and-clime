f2fffc50e4d6b1dc2a931f41df0b4b934beb4425
João Emanuel da Silva Mendonça <81983803+JoaoEmanuell@users.noreply.github.com>
2023-09-24 11:34:47 -0300

    feat: utils/page/bg-images

---
+++ gbbc/src/utils/pages/bg-images/bg-images.ts
@@ -0,0 +1,10 @@
+const pathToBgImages = '/assets/images/biomes/bg-images'
+
+export const bgImages = {
+    amazon: `${pathToBgImages}/amazon.webp`,
+    caatinga: `${pathToBgImages}/caatinga.webp`,
+    cerrado: `${pathToBgImages}/cerrado.webp`,
+    atlanticForest: `${pathToBgImages}/mata-atlantica.webp`,
+    pampa: `${pathToBgImages}/pampa.webp`,
+    pantanal: `${pathToBgImages}/pantanal.webp`,
+}
